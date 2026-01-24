d877bf33dbae525848be991f9d69f1e756252e48
Edge Technosoft pvt. ltd.neha3112-edge <support@edgetechnosoft.com>
2026-01-24 16:08:12 +0530

    Remove GTM

--- index.php
+++ index.php
@@ -22,10 +22,6 @@ $accordionData = [
 
     </style>
 
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5H9ZS4M5"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
 <div id="hero-section">
    <div class="container">
        <div class="banner">
